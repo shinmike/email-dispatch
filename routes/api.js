0af23d04359bd3f62851d81129d058560d68ccba
Michael Shin <shinmike90@gmail.com>
2018-02-01 22:04:34 -0800

    test email endpoints

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var helper = require('sendgrid').mail;
 
-/* GET users listing. */
 router.get('/:action', function (req, res, next) {
 
   var action = req.params.action;
 
   if (action == 'send') { //send an email
-    res.json({
-      confirmation: 'success',
-      action: action
+    var from_email = new helper.Email('shinmike90@gmail.com');
+    var to_email = new helper.Email('shinmike90@gmail.com');
+    var subject = 'TEST!';
+    var content = new helper.Content('text/html', 'Hello, From Email Dispatch!');
+    var mail = new helper.Mail(from_email, subject, to_email, content);
+
+    var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+    var request = sg.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: mail.toJSON(),
     });
+
+    sg.API(request, function (error, response) {
+      if (error) {
+        res.json({
+          confirmation: 'fail',
+          message: error
+        });
+        return
+      }
+      res.json({
+        confirmation: 'success',
+        response: response
+      });
+    });
+
+    
     return;
   }
 

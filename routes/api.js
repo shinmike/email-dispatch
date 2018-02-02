41ac86af4662b74e23ce09cc0d6e2c3db065c554
Michael Shin <shinmike90@gmail.com>
2018-02-02 15:07:12 -0800

    commits made?

--- routes/api.js
+++ routes/api.js
@@ -1,90 +1,25 @@
 var express = require('express');
 var router = express.Router();
-var helper = require('sendgrid').mail;
+var utils = require('../utils');
 
 router.post('/:action', function (req, res, next) {
   var action = req.params.action;
+  if (action == 'send') { // send an email
+    var recipients = req.body.recipients; // comma separated list of emails
+    var list = recipients.split(',');
 
-  if (action == 'send') { //send an email
-    var from_email = new helper.Email('shinmike90@gmail.com');
-    var to_email = new helper.Email(req.body.recipient);
-    var subject = req.body.subject;
-    var content = new helper.Content('text/html', req.body.content);
-    var mail = new helper.Mail(from_email, subject, to_email, content);
-
-    var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-    var request = sg.emptyRequest({
-      method: 'POST',
-      path: '/v3/mail/send',
-      body: mail.toJSON(),
-    });
-
-    sg.API(request, function (error, response) {
-      if (error) {
-        res.json({
-          confirmation: 'fail',
-          message: error
-        });
-        return
-      }
+    utils.Email.sendEmails(list, req.body, function () {
       res.json({
         confirmation: 'success',
-        response: response
+        message: 'Emails sent!'
       });
     });
-
-
     return;
   }
-
   res.json({
     confirmation: 'fail',
     action: 'Invalid action'
   });
 });
-
-router.get('/:action', function (req, res, next) {
-
-    var action = req.params.action;
-
-    if (action == 'send') { //send an email
-      var from_email = new helper.Email('shinmike90@gmail.com');
-      var to_email = new helper.Email('shinmike90@gmail.com');
-      var subject = 'TEST!';
-      var content = new helper.Content('text/html', 'Hello, From Email Dispatch!');
-      var mail = new helper.Mail(from_email, subject, to_email, content);
-
-      var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-      var request = sg.emptyRequest({
-        method: 'POST',
-        path: '/v3/mail/send',
-        body: mail.toJSON(),
-      });
-
-      sg.API(request, function (error, response) {
-        if (error) {
-          res.json({
-            confirmation: 'fail',
-            message: error
-          });
-          return
-        }
-        res.json({
-          confirmation: 'success',
-          response: response
-        });
-      });
-
-
-      return;
-    }
-
-    res.json({
-      confirmation: 'fail',
-      action: 'Invalid action'
-    });
-
-
-  });
 
 module.exports = router;
